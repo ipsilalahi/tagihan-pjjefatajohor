9c0795debf518f0f6691139d5e32309dbe5ac301
ipsilalahi <65601929+ipsilalahi@users.noreply.github.com>
2023-10-31 06:11:01 +0700

    Update index.js
    connection =====> connection2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const startSock = () => {
         auth: state
     })
 
-    sock.ev.on ('connection.update', function(update, connection) {
+    sock.ev.on ('connection.update', function(update, connection2) {
         let _a, _b;
         let connection = update.connection, lastDisconnect = update.lastDisconnect;
         if (connection == 'close') {
